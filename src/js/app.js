e4d961ddc75eb91899b32ddc4003930800b240df
twentytwo777 <torukas07@gmail.com>
2025-10-20 23:58:41 +0400

    Patch Change (0.0.1) 1. Обновлён код в src/js. 2. Фикс бага src/styles.css.

--- src/js/app.js
+++ src/js/app.js
@@ -1,34 +1,46 @@
-function preloadImage(path) {
-    let img = new Image();
+// Constants Variables
+const button = document.querySelector('.preloader-content_button');
+const image_folder = 'src/assets';
+const images = {
+    vk: preload_image(`${image_folder}/vk.svg`),
+    telegram: preload_image(`${image_folder}/tg.svg`),
+    discord: preload_image(`${image_folder}/ds.svg`),
+    github: preload_image(`${image_folder}/git.svg`)
+};
+const image = document.querySelector('.main_content_img');
+// Temporary Variables
+let iterator = document.querySelectorAll('.main_content_item').values();
+let timer = null;
+// Methods
+/**
+ * 
+ * @param {string} path 
+ * @returns {HTMLImageElement}
+ */
+const preload_image = (path) => {
+    const img = new Image();
     img.src = path;
     return img;
-}
-
-// Constants Variables
-const main_items = document.querySelectorAll('.main_content_item');
-const main_content_img = document.querySelector('.main_content_img');
-const folderIMG = './src/assets';
-const images = {
-    vk: preloadImage(`${folderIMG}/vk.svg`), telegram: preloadImage(`${folderIMG}/tg.svg`),
-    discord: preloadImage(`${folderIMG}/ds.svg`), github: preloadImage(`${folderIMG}/git.svg`)
 };
-const preloader = document.querySelector('.preloader'), button = document.querySelector('.preloader-content_button');
-const searchresult = /\#(?<id>[A-Za-z0-9_\-]{11})/.exec(window.location.hash);
-const videoId = searchresult ? searchresult.groups.id : "1PuWLayNJKc";
-// Temporary Variables
-let timer = null;
 
-YT.ready(_ => {
+YT.ready(() => {
+    const hash = /\#(?<id>[A-Za-z0-9_\-]{11})/.exec(window.location.hash);
+    const video_id = hash ? hash.groups.id : '1PuWLayNJKc';
     player = new YT.Player('player', {
-        videoId: videoId,
-        playerVars: { 'autoplay': 0, 'controls': 0, 'loop': 1, 'playlist': videoId },
+        videoId: video_id,
+        playerVars: {
+            autoplay: 0,
+            controls: 0,
+            loop: 1,
+            playlist: video_id
+        },
         events: {
-            'onReady': e => {
+            onReady: (e) => {
                 e.target.setVolume(25);
                 button.innerText = 'Click';
                 button.classList.add('ready');
-                button.onclick = _ => {
-                    preloader.remove()
+                button.onclick = () => {
+                    document.querySelector('.preloader').remove();
                     player.unMute();
                     player.playVideo();
                 };
@@ -37,20 +49,22 @@ YT.ready(_ => {
     });
 });
 
-const enterMouseItems = items => {
-    items.forEach(el => {
-        el.onmouseenter = _ => {
-            timer = setTimeout(_ => {
-                main_content_img.src = images[el.id].src;
-                main_content_img.classList.add('active');
-            }, 200);
-        };
+for (;;) {
+    const next = iterator.next();
+    if (next.done) {
+        iterator = null;
+        break;
+    };
 
-        el.onmouseout = _ => {
-            main_content_img.classList.remove('active');
-            clearTimeout(timer);
-        };
-    });
+    next.value.onmouseenter = () => {
+        timer = setTimeout(() => {
+            image.src = images[next.value.id].src;
+            image.classList.add('active');
+        }, 200);
+    };
+
+    next.value.onmouseout = () => {
+        clearTimeout(timer);
+        image.classList.remove('active');
+    };
 };
-
-enterMouseItems(main_items);
