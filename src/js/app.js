984730f950a5574f9734f02530a8a0139d439fbb
twentytwo777 <torukas07@gmail.com>
2026-07-14 01:14:13 +0400

    Major Update (1.0.1) 1. Переписан сайт с нуля 2. Добавлены более свежие лого соц. сетей

--- src/js/app.js
+++ src/js/app.js
@@ -1,70 +1,53 @@
-// Methods
-/**
- * 
- * @param {string} path 
- * @returns {HTMLImageElement}
- */
-const preload_image = (path) => {
-    const img = new Image();
-    img.src = path;
-    return img;
-};
-// Constants Variables
-const button = document.querySelector('.preloader-content_button');
-const image_folder = 'src/assets';
+const linkIcon = document.querySelector('.content-link-icon');
 const images = {
-    vk: preload_image(`${image_folder}/vk.svg`),
-    telegram: preload_image(`${image_folder}/tg.svg`),
-    discord: preload_image(`${image_folder}/ds.svg`),
-    github: preload_image(`${image_folder}/git.svg`)
+    vk: 'src/assets/vk.svg',
+    telegram: 'src/assets/telegram.svg',
+    discord: 'src/assets/discord.svg',
+    github: 'src/assets/github.svg'
 };
-const image = document.querySelector('.main_content_img');
-// Temporary Variables
-let iterator = document.querySelectorAll('.main_content_item').values();
+
+let iterator = document.querySelectorAll('.content-link').values();
 let timer = null;
 
 YT.ready(() => {
     const hash = /\#(?<id>[A-Za-z0-9_\-]{11})/.exec(window.location.hash);
-    const video_id = hash ? hash.groups.id : '1PuWLayNJKc';
-    player = new YT.Player('player', {
-        videoId: video_id,
+    const videoId = hash ? hash.groups.id : '1PuWLayNJKc';
+    new YT.Player('player', {
+        videoId,
         playerVars: {
-            autoplay: 0,
-            controls: 0,
             loop: 1,
-            playlist: video_id
+            controls: 0,
+            autoplay: 0,
+            playlist: videoId
         },
         events: {
-            onReady: (e) => {
-                e.target.setVolume(25);
-                button.innerText = 'Click';
-                button.classList.add('ready');
-                button.onclick = () => {
-                    document.querySelector('.preloader').remove();
-                    player.unMute();
-                    player.playVideo();
-                };
+            onReady(e) {
+                const preloaderButton = document.querySelector('.preloader-btn');
+                const preloader = document.querySelector('.preloader');
+                const preloaderOverlay = document.querySelector('.preloader-overlay');
+                preloaderButton.addEventListener('click', () => {
+                    e.target.playVideo();
+                    e.target.setVolume(25);
+                    preloader.classList.add('active');
+                    preloaderOverlay.addEventListener('animationend', () => {
+                        preloader.remove();
+                    });
+                });
             }
         }
     });
 });
 
-for (;;) {
-    const next = iterator.next();
-    if (next.done) {
-        iterator = null;
-        break;
-    };
-
-    next.value.onmouseenter = () => {
+for (let next = iterator.next(); !next.done; next = iterator.next()) {
+    next.value.addEventListener('mouseenter', () => {
+        linkIcon.src = images[next.value.dataset.id];
         timer = setTimeout(() => {
-            image.src = images[next.value.id].src;
-            image.classList.add('active');
+            linkIcon.classList.add('active');
         }, 200);
-    };
+    });
 
-    next.value.onmouseout = () => {
+    next.value.addEventListener('mouseout', () => {
         clearTimeout(timer);
-        image.classList.remove('active');
-    };
+        linkIcon.classList.remove('active');
+    });
 };
